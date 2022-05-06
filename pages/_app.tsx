390877d134ba762894f6b7d1ea1a58dbced12b16
VoduDay <andriy.maniak@gmail.com>
2022-05-07 02:13:57 +0300

    update routes in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from "styled-components";
-import theme from "../styles/theme";
+import theme from "~/styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
