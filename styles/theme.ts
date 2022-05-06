a25e1c081dd0889a5238aa85aa334c389c108c11
VoduDay <andriy.maniak@gmail.com>
2022-05-07 01:48:31 +0300

    create theme and add theme provider

---
+++ styles/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    white: "#fff",
+    black: "#000",
+  },
+  media: {
+    widescreen: "@media (max-width: 1200px)",
+    desktop: "@media (max-width: 1024px)",
+    tablet: "@media (max-width: 768px)",
+    mobile: "@media (max-width: 480px)",
+  },
+}
+
+export default theme;
+
