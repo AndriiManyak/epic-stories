17f2c0f0ea9365a0db51513edfafee9190b4a11b
VoduDay <andriy.maniak@gmail.com>
2022-05-07 19:23:24 +0300

    update InventoryItem component

--- components/invertory/InventoryItem.tsx
+++ components/invertory/InventoryItem.tsx
@@ -1,19 +1,39 @@
 import styled from "styled-components";
-import sword from "~/public/test/sword-icon.svg";
 import Image from "next/image";
 
-export const InventoryItem = () => {
+type Props = {
+  itemImage?: string | HTMLImageElement;
+};
+
+export const InventoryItem = ({ itemImage }: Props) => {
   return (
     <InventoryContainer>
-      <Image src={sword} alt="sword" />
+      {itemImage ? (
+        <ImageContainer>
+          <Image
+            src={itemImage}
+            alt="sword"
+            layout="fill"
+            objectFit="contain"
+          />
+        </ImageContainer>
+      ) : (
+        <span>empty</span>
+      )}
     </InventoryContainer>
   );
 };
 
 const InventoryContainer = styled.div`
-  height: 50px;
-  width: 50px;
+  height: 100%;
+  width: 100%;
 
   padding: 5px;
-  border: 2px solid ${(props) => props.theme.colors.black};
+  border: 2px solid ${({ theme }) => theme.colors.black};
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
 `;
