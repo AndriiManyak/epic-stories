b19fe95967502adcbc8c3deccf315df19d097f21
VoduDay <andriy.maniak@gmail.com>
2022-05-07 17:44:04 +0300

    update code style in components

--- components/invertory/InventoryItem.tsx
+++ components/invertory/InventoryItem.tsx
@@ -3,13 +3,17 @@ import sword from "~/public/test/sword-icon.svg";
 import Image from "next/image";
 
 export const InventoryItem = () => {
-  return <InventoryContainer><Image src={sword} alt="sword"/></InventoryContainer>
-}
+  return (
+    <InventoryContainer>
+      <Image src={sword} alt="sword" />
+    </InventoryContainer>
+  );
+};
 
 const InventoryContainer = styled.div`
-	height: 50px;
-	width: 50px;
-	
-	padding: 5px;
-	border: 2px solid ${props => props.theme.colors.black};
-`
+  height: 50px;
+  width: 50px;
+
+  padding: 5px;
+  border: 2px solid ${(props) => props.theme.colors.black};
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import type { NextPage } from 'next'
-import {InventoryBlock} from "../components/invertory/InventoryBlock";
+import type { NextPage } from "next";
+import { Inventory } from "~/components/invertory/Inventory";
+import { Gear } from "~/components/invertory/Gear";
 
 const Home: NextPage = () => {
-  return <div><InventoryBlock /></div>
-}
+  return (
+    <div>
+      <Inventory size={25} />
+      <Gear />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
