aaec33966acfd91945a08e954869739ac1a80ea0
VoduDay <andriy.maniak@gmail.com>
2022-05-09 00:49:26 +0300

    create InventoryItemComponent

---
+++ components/inventory/InventoryItemContentMenu.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+type Props = {
+  isVisible: boolean;
+};
+
+export const InventoryItemContentMenu = ({ isVisible }: Props) => {
+  return (
+    <Container isVisible={isVisible}>
+      <Menu>
+        <Option>Take off</Option>
+      </Menu>
+    </Container>
+  );
+};
+
+const Container = styled.div<{ isVisible: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 1;
+
+  background: white;
+  color: black;
+  display: ${({ isVisible }) => (isVisible ? "block" : "none")};
+  border: 1px solid ${({ theme }) => theme.colors.primaryBorder};
+`;
+
+const Menu = styled.div`
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px 0;
+`;
+
+const Option = styled.div`
+  padding: 5px 10px;
+`;
