fc4bd80c4974e8a1ba1df346916e758fac7e59a5
VoduDay <andriy.maniak@gmail.com>
2022-05-16 19:16:54 +0300

    add inventory page

---
+++ pages/inventory.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { GearComponent } from "~/components/inventory/gear/GearComponent";
+
+const Inventory: NextPage = () => {
+  return (
+    <div>
+      <GearComponent />
+    </div>
+  );
+};
+
+export default Inventory;
