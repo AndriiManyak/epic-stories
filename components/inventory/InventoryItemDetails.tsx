2f0c057da0b1b951ace49ac5d352f3947456ccca
VoduDay <andriy.maniak@gmail.com>
2022-05-16 18:03:19 +0300

    remove image from InventoryItemDetails

--- components/inventory/InventoryItemDetails.tsx
+++ components/inventory/InventoryItemDetails.tsx
@@ -1,6 +1,5 @@
 import { InventoryItem } from "~/types/inventoryItem";
 import styled from "styled-components";
-import Image from "next/image";
 import { ItemCharacteristics } from "~/components/inventory/ItemCharacteristics";
 
 type Props = {
@@ -10,21 +9,13 @@ type Props = {
 export const InventoryItemDetails = ({ item }: Props) => {
   return (
     <Container>
-      <ItemMainDetails>
-        <ItemImageWrapper>
-          <Image
-            src={item.image}
-            alt={item.name}
-            layout="fill"
-            objectFit="cover"
-          />
-        </ItemImageWrapper>
+      <ItemName>{item.name}</ItemName>
 
+      <ItemDetails>
         <ItemInfoWrapper>
-          <ItemName>{item.name}</ItemName>
           <ItemDescription>{item.description}</ItemDescription>
         </ItemInfoWrapper>
-      </ItemMainDetails>
+      </ItemDetails>
 
       <ItemCharacteristics characteristics={item.characteristics} />
     </Container>
@@ -34,26 +25,20 @@ export const InventoryItemDetails = ({ item }: Props) => {
 const Container = styled.div`
   color: ${({ theme }) => theme.colors.white};
   background: ${({ theme }) => theme.colors.primary};
-  padding: 10px;
   border: 2px solid ${({ theme }) => theme.colors.primaryBorder};
 `;
 
-const ItemMainDetails = styled.div`
-  margin-bottom: 20px;
-  display: flex;
+const ItemName = styled.p`
+  padding: 5px 10px;
 `;
 
-const ItemImageWrapper = styled.div`
-  margin-right: 10px;
-  height: 80px;
-  width: 80px;
-  min-width: 80px;
+const ItemDetails = styled.div`
+  margin-bottom: 20px;
+  display: flex;
 
-  position: relative;
+  padding: 10px;
 `;
 
 const ItemInfoWrapper = styled.div``;
-
-const ItemName = styled.p``;
 
 const ItemDescription = styled.p``;
