856d979c82a83b940e2c94b3664ac6d77013f42e
VoduDay <andriy.maniak@gmail.com>
2022-05-17 00:11:02 +0300

    update InventoryTabs logic

--- components/inventory/inventoryPage/InventoryTab.tsx
+++ components/inventory/inventoryPage/InventoryTab.tsx
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 
 type Props = {
+  isActive: boolean;
   icon: string;
   title: string;
+  onClick: () => void;
 };
 
-export const InventoryTab = ({ icon, title }: Props) => {
+export const InventoryTab = ({ icon, title, isActive, onClick }: Props) => {
   return (
-    <Container>
+    <Container isActive={isActive} onClick={onClick}>
       <IconWrapper>{icon}</IconWrapper>
       <Title>{title}</Title>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ isActive: boolean }>`
   display: flex;
+
+  padding: 5px;
+  cursor: pointer;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+  opacity: ${({ isActive }) => (isActive ? 1 : 0.5)};
 `;
 
 const IconWrapper = styled.div``;
--- components/inventory/inventoryPage/InventoryTabs.tsx
+++ components/inventory/inventoryPage/InventoryTabs.tsx
@@ -1,29 +1,49 @@
 import styled from "styled-components";
 import { InventoryTab } from "~/components/inventory/inventoryPage/InventoryTab";
 import { GearComponent } from "~/components/inventory/gear/GearComponent";
+import { ReactNode, useState } from "react";
+import { Inventory } from "~/components/inventory/Inventory";
 
-const tabs: { icon: string; title: string }[] = [
+const tabs: { icon: string; title: string; content: ReactNode }[] = [
   {
     title: "Gear",
     icon: "Gear",
+    content: <GearComponent />,
   },
   {
     title: "Backpack",
     icon: "Backpack",
+    content: <Inventory size={20} />,
   },
 ];
 
 export const InventoryTabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const selectTab = (index: number) => {
+    setActiveTab(index);
+  };
+
   return (
     <Container>
       <TabsList>
         {tabs.map((tab, index) => (
-          <InventoryTab key={index} icon={tab.icon} title={tab.title} />
+          <InventoryTab
+            key={index}
+            icon={tab.icon}
+            title={tab.title}
+            isActive={index === activeTab}
+            onClick={() => selectTab(index)}
+          />
         ))}
       </TabsList>
 
       <TabContent>
-        <GearComponent />
+        {tabs.map((tab, index) => (
+          <ContentWrapper key={index} isShown={index === activeTab}>
+            {tab.content}
+          </ContentWrapper>
+        ))}
       </TabContent>
     </Container>
   );
@@ -43,4 +63,8 @@ const TabsList = styled.div`
 const TabContent = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const ContentWrapper = styled.div<{ isShown: boolean }>`
+  display: ${({ isShown }) => (isShown ? "block" : "none")};
 `;
